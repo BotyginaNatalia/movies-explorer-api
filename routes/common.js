b74b753c5ad6eb210bf5e4cf639490771a984f02
Natalia <n.botygina@mail.ru>
2022-09-06 18:28:33 +0300

    new common route

---
+++ routes/common.js
@@ -0,0 +1,24 @@
+const { celebrate, Joi } = require('celebrate');
+const router = require('express').Router();
+const { login, createNewUser } = require('../controllers/users');
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  createNewUser,
+);
+
+module.exports = router;
