de26163078b5d0be1c45f47a7cee51115706774c
Natalia <n.botygina@mail.ru>
2022-10-27 09:37:54 +0300

    changes

--- models/movie.js
+++ models/movie.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const movieSchema = new mongoose.Schema({
 
@@ -39,12 +40,11 @@ const movieSchema = new mongoose.Schema({
 
   trailerLink: {
     type: String,
-    required: false,
     validate: {
-      validator(URL) {
-        return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\\+\\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\\+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/i.test(URL);
-      },
+      validator: (v) => validator.isURL(v),
+      message: 'Не соответсвует формату URL-адреса',
     },
+    required: true,
   },
 
   thumbnail: {
