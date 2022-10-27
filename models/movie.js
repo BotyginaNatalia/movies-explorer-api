38264d6bec021e16c1208bee8d3ed60c24ffa946
Natalia <n.botygina@mail.ru>
2022-10-27 08:16:40 +0300

    changes

--- models/movie.js
+++ models/movie.js
@@ -39,7 +39,7 @@ const movieSchema = new mongoose.Schema({
 
   trailerLink: {
     type: String,
-    required: true,
+    required: false,
     validate: {
       validator(URL) {
         return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\\+\\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\\+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/i.test(URL);
